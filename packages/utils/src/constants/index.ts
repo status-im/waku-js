a014535313d67f67f91023c081281a0e4426c83d
danisharora099 <danisharora099@gmail.com>
2023-04-19 13:08:37 +0530

    wip

---
+++ packages/utils/src/constants/index.ts
@@ -0,0 +1,8 @@
+export * from "./enr.js";
+export * from "./message-encryption.js";
+export * from "./relay.js";
+
+/**
+ * DefaultPubSubTopic is the default gossipsub topic to use for Waku.
+ */
+export const DefaultPubSubTopic = "/waku/2/default-waku/proto";
